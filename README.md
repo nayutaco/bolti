28e09b222034c7b33086cb37752c14d50dca769d
tock203 <tick@tock.tech>
2019-02-25 12:39:52 +0900

    Stop building ptarmigan in build.rs

--- README.md
+++ README.md
@@ -5,6 +5,6 @@ WIP: A CLI tool for send/receive BOLT messages interactively
 ```sh
 git clone https://github.com/nayutaco/bolti.git --recursive
 cd bolti
-make -C ptarmigan/libs
+make full
 LIBCLANG_PATH=<where libclang.so exists> cargo build
 ```
--- build.rs
+++ build.rs
@@ -15,41 +15,13 @@ fn main() {
     .write_to_file(out_path.join("bindings.rs"))
     .expect("Couldn't write bindings!");
 
-  cc::Build::new()
-    .file("ptarmigan/ln/ln.c")
-    .file("ptarmigan/ln/ln_noise.c")
-    .file("ptarmigan/ln/ln_node.c")
-    .file("ptarmigan/utl/utl_buf.c")
-    .file("ptarmigan/utl/utl_log.c")
-    .file("ptarmigan/utl/utl_time.c")
-    .file("ptarmigan/utl/utl_str.c")
-    .file("ptarmigan/btc/btc_keys.c")
-    .file("ptarmigan/btc/btc_crypto.c")
-    .file("ptarmigan/libs/mbedtls/library/chachapoly.c")
-    .file("ptarmigan/libs/mbedtls/library/bignum.c")
-    .file("ptarmigan/libs/mbedtls/library/ecp.c")
-    .file("ptarmigan/libs/mbedtls/library/poly1305.c")
-    .file("ptarmigan/libs/mbedtls/library/chacha20.c")
-    .file("ptarmigan/libs/mbedtls/library/md.c")
-    .file("ptarmigan/libs/mbedtls/library/hkdf.c")
-    .file("ptarmigan/libs/mbedtls/library/sha256.c")
-    .file("ptarmigan/libs/mbedtls/library/ecp_curves.c")
-    .file("ptarmigan/libs/mbedtls/library/ctr_drbg.c")
-    .file("ptarmigan/libs/mbedtls/library/error.c")
-    .file("ptarmigan/libs/mbedtls/library/platform.c")
-    .file("ptarmigan/libs/mbedtls/library/platform_util.c")
-    .file("ptarmigan/libs/mbedtls/library/aes.c")
-    .file("ptarmigan/libs/mbedtls/library/md5.c")
-    .file("ptarmigan/libs/mbedtls/library/md_wrap.c")
-    .file("ptarmigan/libs/mbedtls/library/sha1.c")
-    .file("ptarmigan/libs/mbedtls/library/sha512.c")
-    .file("ptarmigan/libs/mbedtls/library/aesni.c")
-    .file("ptarmigan/libs/mbedtls/library/ripemd160.c")
-    .file("ptarmigan/libs/mbedtls/library/entropy.c")
-    .file("ptarmigan/libs/mbedtls/library/entropy_poll.c")
-    .file("ptarmigan/libs/mbedtls/library/timing.c")
-    .include("ptarmigan/utl")
-    .include("ptarmigan/btc")
-    .include("ptarmigan/libs/install/include")
-    .compile("ptarmigan");
+  println!("cargo:rustc-link-search=native=ptarmigan/btc");
+  println!("cargo:rustc-link-search=native=ptarmigan/ln");
+  println!("cargo:rustc-link-search=native=ptarmigan/utl");
+  println!("cargo:rustc-link-search=native=ptarmigan/libs/install/lib");
+  println!("cargo:rustc-link-lib=static=btc");
+  println!("cargo:rustc-link-lib=static=ln");
+  println!("cargo:rustc-link-lib=static=utl");
+  println!("cargo:rustc-link-lib=static=mbedcrypto");
+  println!("cargo:rustc-link-lib=static=stdc++");
 }
